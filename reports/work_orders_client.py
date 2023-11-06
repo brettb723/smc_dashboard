9b5d95e4f48c3386e238dd6a8382a38cf09be517
brett <bbrockway@standardmanagementco.com>
2023-11-06 16:43:34 -0500

    adding up filters to the work orders by client report

--- reports/work_orders_client.py
+++ reports/work_orders_client.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 from utils.data_fetcher import fetch_appfolio_report
+from streamlit import session_state as state
+
+if 'selected_owner' not in state:
+    state.selected_owner = 'All'
 
 def process_data(work_order_data, property_directory_data):
     # Convert data to pandas DataFrames
@@ -14,62 +18,91 @@ def process_data(work_order_data, property_directory_data):
     
     # Merge filtered dataframes on 'PropertyId'
     merged_df = filtered_work_order_df.merge(property_directory_df, on='PropertyId', how='left')
+
+    merged_df.rename(columns={
+        'PropertyAddress_x': 'PropertyAddress',
+        'PropertyCity_x': 'PropertyCity',
+        'PropertyZip_x': 'PropertyZip'
+    }, inplace=True)
     return merged_df
 
+def calculate_aging(row, current_date):
+    aging_days = (current_date - pd.to_datetime(row['CreatedAt'])).days
+    if aging_days <= 30:
+        return '0-30'
+    elif 31 <= aging_days <= 60:
+        return '31-60'
+    elif 61 <= aging_days <= 90:
+        return '61-90'
+    else:
+        return '90+'
 
 
 def display_report():
     st.title("Work Order Aging Report")
 
-    # Fetch work_order data
+    # Fetch data
     work_order_data = fetch_appfolio_report("work_order")
-    if not work_order_data:
-        st.error("Failed to fetch work order data. Please check the connection or credentials.")
-        return
-
-    # Fetch property_directory data
     property_directory_data = fetch_appfolio_report("property_directory")
-    if not property_directory_data:
-        st.error("Failed to fetch property directory data. Please check the connection or credentials.")
+    if not work_order_data or not property_directory_data:
+        st.error("Failed to fetch data. Please check the connection or credentials.")
         return
 
-    # Process the fetched data
+    # Process data
     df = process_data(work_order_data, property_directory_data)
+    current_date = pd.Timestamp('now')
+    df['Aging'] = df.apply(lambda row: calculate_aging(row, current_date), axis=1)
 
-    # Define aging buckets
+    # Filter widgets
+    unique_owners = df['Owners'].unique()
+    selected_owner = st.selectbox('Select a Client:', ['All'] + list(unique_owners))
     aging_buckets = ['0-30', '31-60', '61-90', '90+']
+    selected_aging_bucket = st.selectbox('Select an Aging Bucket:', ['All'] + aging_buckets)
+
+    # Apply filters for summary table
+    summary_df = df.copy()
+    if selected_owner != 'All':
+        summary_df = summary_df[summary_df['Owners'] == selected_owner]
+    if selected_aging_bucket != 'All':
+        summary_df = summary_df[summary_df['Aging'] == selected_aging_bucket]
+
+    # Generate the hyperlink for each service request number
+    # But display it with WorkOrderNumber text
+    df['WorkOrderNumber'] = df.apply(
+        lambda x: f'<a href="https://standardmgmtco.appfolio.com/maintenance/service_requests/{x["ServiceRequestNumber"]}" target="_blank">{x["WorkOrderNumber"]}</a>', axis=1
+    )
+
+    # Ensure you convert the column to string if not already
+    df['WorkOrderNumber'] = df['WorkOrderNumber'].astype(str) 
+
+    # Generate the summary aging table
+    aging_summary = summary_df.groupby(['Owners', 'Aging']).size().reset_index(name='Counts')
+
+    # Display the summary aging table
+    st.write("Summary Aging Table:")
+    st.dataframe(aging_summary.pivot(index='Owners', columns='Aging', values='Counts').fillna(0))
+
+    # Apply filters for detailed table
+    if selected_owner != 'All':
+        df = df[df['Owners'] == selected_owner]
+    if selected_aging_bucket != 'All':
+        df = df[df['Aging'] == selected_aging_bucket]
+
+    # Define the columns to display in the detailed report
+    detailed_columns = [
+        'PropertyAddress', 'PropertyCity', 'PropertyZip', 'WorkOrderNumber',
+        'WorkOrderType', 'ServiceRequestDescription', 'JobDescription',
+        'VendorTrade', 'Status', 'UnitName', 'Owners', 'Aging'
+    ]
     
-    # Create an empty DataFrame for the aging report
-    aging_report_df = pd.DataFrame(columns=['Owners'] + aging_buckets)
+    # Select only the specified columns for the detailed report
+    detailed_df = df[detailed_columns]
 
-    # Group the work orders by Owner and calculate the aging
-    for owner, group in df.groupby('Owners'):
-        # Initialize a new row with the owner's name and zeros for each bucket
-        new_row = {'Owners': owner, **{bucket: 0 for bucket in aging_buckets}}
+    # Display the detailed table based on filters
+    st.write("Detailed Report:")
+    st.write(detailed_df.to_html(escape=False, index=False), unsafe_allow_html=True)
 
-        # Calculate the count for each aging bucket
-        for _, work_order in group.iterrows():
-            aging_days = (pd.Timestamp('now') - pd.to_datetime(work_order['CreatedAt'])).days
-            if aging_days <= 30:
-                new_row['0-30'] += 1
-            elif 31 <= aging_days <= 60:
-                new_row['31-60'] += 1
-            elif 61 <= aging_days <= 90:
-                new_row['61-90'] += 1
-            else:
-                new_row['90+'] += 1
 
-        # Append the new row to the aging report DataFrame
-        aging_report_df = pd.concat([aging_report_df, pd.DataFrame([new_row])], ignore_index=True)
-
-    # Calculate totals for each column (excluding the 'Owners' column)
-    totals = aging_report_df.drop('Owners', axis=1).sum()
-    totals['Owners'] = 'Total'  # Add 'Owners' column with 'Total' as the value
-    # Append totals row to the DataFrame
-    aging_report_df = pd.concat([aging_report_df, pd.DataFrame([totals])], ignore_index=True)
-
-    # Display the aging report DataFrame with sortable headers
-    st.dataframe(aging_report_df.set_index('Owners'), width=700, height=500)
 
 # Run the report display function
 if __name__ == "__main__":
